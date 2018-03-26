ebe09d8ebe266186e38b0e0711e5ddbd01c3e5c4
copycoconut <divshop01@gmail.com>
2018-03-26 11:57:20 +0700

    182631157

--- bot.php
+++ bot.php
@@ -58,13 +58,15 @@ $content = file_get_contents('php://input');
 // แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
 $events = json_decode($content, true);
 if(!is_null($events)){
-    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
+    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน และอื่นๆด้วย
     $replyToken = $events['events'][0]['replyToken'];
+	$groupId = $events['events'][0]['groupId'];
+	
 }
 
 // ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
 //$textMessageBuilder = new TextMessageBuilder(json_encode($events));
-$textMessageBuilder = new TextMessageBuilder($events);
+$textMessageBuilder = new TextMessageBuilder($groupId);
 
 //l ส่วนของคำสั่งตอบกลับข้อความ
 $response = $bot->replyMessage($replyToken,$textMessageBuilder);
