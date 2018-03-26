3c5a5dfe66737e109fe0d470e80fa59a4e057a0b
copycoconut <divshop01@gmail.com>
2018-03-26 16:52:17 +0700

    182631652

---
+++ bot1.php
@@ -0,0 +1,108 @@
+<?php
+// กรณีต้องการตรวจสอบการแจ้ง error ให้เปิด 3 บรรทัดล่างนี้ให้ทำงาน กรณีไม่ ให้ comment ปิดไป
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ 
+// include composer autoload
+require_once 'vendor/autoload.php';
+ 
+// การตั้งเกี่ยวกับ bot
+require_once 'config/bot_settings.php';
+ 
+// กรณีมีการเชื่อมต่อกับฐานข้อมูล
+//require_once("config/dbconnect.php");
+ 
+///////////// ส่วนของการเรียกใช้งาน class ผ่าน namespace
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+//use LINE\LINEBot\Event;
+//use LINE\LINEBot\Event\BaseEvent;
+//use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\MessageBuilder;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
+use LINE\LINEBot\MessageBuilder\AudioMessageBuilder;
+use LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
+use LINE\LINEBot\ImagemapActionBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\AreaBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder ;
+use LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder;
+use LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder;
+use LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder;
+use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use LINE\LINEBot\TemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\DatetimePickerTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\MessagetemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\getbackTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder;
+ 
+// เชื่อมต่อกับ LINE Messaging API
+$httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
+$bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
+ 
+// คำสั่งรอรับการส่งค่ามาของ LINE Messaging API
+$content = file_get_contents('php://input');
+ 
+// แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
+$events = json_decode($content, true);
+if(!is_null($events)){
+    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน และอื่นๆด้วย
+    $replyToken = $events['events'][0]['replyToken'];
+	$typeMessage = $events['events'][0]['message']['type'];
+    $userMessage = $events['events'][0]['message']['text'];
+	$idMessage = $events['events'][0]['message']['id'];
+	$userId = $events['events'][0]['source']['userId'];
+	$groupId = $events['events'][0]['source']['groupId'];
+	$typeId = $events['events'][0]['source']['type'];
+    $userMessage = strtolower($userMessage);
+		if($typeMessage=='sticke'){
+			$stickerId = $events['events'][0]['message']['stickerId'];
+			$packageId = $events['events'][0]['message']['packageId'];
+
+		}
+	$timestamp = $events['events'][0]['timestamp'];
+
+	//รวมข้อความไว้แสดงผล
+	$all = $replyToken.' -'.$typeMessage.' -'.$userMessage.' -'.$idMessage;
+	
+}
+
+
+// ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
+//$textMessageBuilder = new TextMessageBuilder(json_encode($events));
+$textMessageBuilder = new TextMessageBuilder($all.' =>'.json_encode($events));
+/*
+//l ส่วนของคำสั่งตอบกลับข้อความ
+$response = $bot->replyMessage($replyToken,$textMessageBuilder);
+if ($response->isSucceeded()) {
+    echo 'Succeeded!';
+    return;
+}
+ 
+// Failed
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
+*/
+//ทดสอบ getMessageContent
+
+$response = $bot->getMessageContent($idMessage);
+if ($response->isSucceeded()) {
+    $tempfile = tmpfile();
+    fwrite($tempfile, $response->getRawBody());
+} else {
+    error_log($response->getHTTPStatus() . ' ' . $response->getRawBody());
+}
+
+?>
